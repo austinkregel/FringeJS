a0f96337197f2fb722ca85965874119af3bd2648
Austin Kregel <austin@kbcomp.co>
2019-03-25 02:05:52 -0400

    Remove the error handling...

--- tests/Exceptions/HandlerTest.js
+++
@@ -1,4 +0,0 @@
-const chai = require('chai');
-const assert = require('assert');
-const Handler = require('../../src/Exceptions/Handler');
-
--- tests/Exceptions/StacktraceTest.js
+++
@@ -1,5 +0,0 @@
-const chai = require('chai');
-const assert = require('assert');
-const Stacktrace = require('../../src/Exceptions/Stacktrace');
-
-// Todo: actually cover the stacktrace bits...

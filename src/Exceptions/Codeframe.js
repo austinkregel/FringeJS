521f6b7a80bcb45e32088111976722c69681625e
Austin Kregel <austin@kbcomp.co>
2019-03-25 01:30:34 -0400

    Remove error handler all together since it sucks :man_facepalming:

--- src/Exceptions/Codeframe.js
+++
@@ -1,8 +0,0 @@
-module.exports = class Codeframe {
-    constructor(file, line, code, frame) {
-        this.file = file;
-        this.line = line;
-        this.code = code;
-        this.frame = frame;
-    }
-}
